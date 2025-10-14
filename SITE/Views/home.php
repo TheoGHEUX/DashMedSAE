15405e81e18dfc00393ccd8c766215198405ef6f
Ali Uysun <ali.uysun@etu.univ-amu.fr>
2025-10-14 14:10:19 +0200

    Update home.php

--- SITE/Views/home.php
+++ SITE/Views/home.php
@@ -28,7 +28,7 @@ $pageScripts = $pageScripts ?? [];
 include __DIR__ . '/partials/head.php';
 ?>
 <body>
-<?php include __DIR__ . '/partials/header.php'; ?>
+<?php include __DIR__ . '/partials/headerPublic.php'; ?>
 <main>
     <section class="hero">
         <div class="container hero__inner">
