50994432220ebac10c0c1ff70e8a55b7f6a625f2
Ali Uysun <ali.uysun@etu.univ-amu.fr>
2025-10-14 14:10:39 +0200

    Update legal-notices.php

--- SITE/Views/legal-notices.php
+++ SITE/Views/legal-notices.php
@@ -25,7 +25,7 @@ $pageScripts = [];
 include __DIR__ . '/partials/head.php';
 ?>
 <body>
-<?php include __DIR__ . '/partials/header.php'; ?>
+<?php include __DIR__ . '/partials/headerPublic.php'; ?>
 
 <main class="content">
     <div class="container">
