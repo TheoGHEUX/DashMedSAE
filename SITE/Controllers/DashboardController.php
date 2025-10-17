8b25c18f777da3e3ec06b6841c33cc7666b49c06
Alexis FABRE <alexis.fabre@etu.univ-amu.fr>
2025-10-17 16:00:50 +0200

    Add PHPDoc comments to DashboardController
    Added PHPDoc comments for the DashboardController class and its index method.

--- SITE/Controllers/DashboardController.php
+++ SITE/Controllers/DashboardController.php
@@ -1,7 +1,16 @@
 <?php
 namespace Controllers;
 
+/**
+ * Contrôleur pour le vrai dashboard avec graphiques et statistiques
+ * 
+*/
 final class DashboardController {
+    /**
+     * Affiche la page du tableau de bord avec graphiques et infos patients
+     * 
+     * @return void
+     */
     public function index(): void {
         if (empty($_SESSION['user'])) {
             header('Location: /login');
